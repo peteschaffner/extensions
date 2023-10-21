0c3315b5c096b7e893cb3db8821b866de19a2e93
Pete Schaffner <peteschaffner@icloud.com>
2023-10-21 14:44:14 +0200

    Change unread notifications menubar icon

--- extensions/linear/src/unread-notifications.tsx
+++ extensions/linear/src/unread-notifications.tsx
@@ -12,7 +12,7 @@ import {
 import { NotificationResult } from "./api/getNotifications";
 import { updateNotification } from "./api/updateNotification";
 import View from "./components/View";
-import { getNotificationMenuBarIcon, getNotificationMenuBarTitle, getNotificationTitle } from "./helpers/notifications";
+import { getNotificationMenuBarTitle, getNotificationTitle } from "./helpers/notifications";
 import { getUserIcon } from "./helpers/users";
 import useNotifications from "./hooks/useNotifications";
 
@@ -61,7 +61,7 @@ function UnreadNotifications() {
   return (
     <MenuBarExtra
       title={getNotificationMenuBarTitle(unreadNotifications)}
-      icon={getNotificationMenuBarIcon(unreadNotifications)}
+      icon={{ source: { light: "light/linear.svg", dark: "dark/linear.svg" } }}
       isLoading={isLoadingNotifications}
     >
       <MenuBarExtra.Section>
